d0d1fe90315444a929f73456dba6202ebdfbffb6
xs-mostakim <mim@xpeedstudio.com>
2022-12-13 10:41:20 +0600

    refunded order notification fixed

--- includes/Admin/Emails/Woocommerce/RefundOrder.php
+++ includes/Admin/Emails/Woocommerce/RefundOrder.php
@@ -11,6 +11,8 @@ class RefundOrder {
 	{
 		add_action('woocommerce_email', [$this,'remove_woocommerce_emails']);
 		add_filter('woocommerce_order_status_refunded',[$this,'orderRefund'],10,2);
+		add_filter('woocommerce_order_fully_refunded_notification',[$this,'orderRefund'],10,2);
+		add_filter('woocommerce_order_partially_refunded_notification',[$this,'orderRefund'],10,2);
 	}
 
 	public function remove_woocommerce_emails($email_class) {
