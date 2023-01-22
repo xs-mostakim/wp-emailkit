1ce7444e0c64e23906d0eb4bd2395f742a0babd1
xs-mostakim <mim@xpeedstudio.com>
2023-01-22 15:30:09 +0600

    admin included

--- includes/Admin/metafield/StyleLoad.php
+++ includes/Admin/metafield/StyleLoad.php
@@ -10,6 +10,7 @@ class StyleLoad {
             if (!is_user_logged_in() || !current_user_can('publish_posts')) {
                 return;
             }
+           // add_action('admin_enqueue_scripts', [$this, 'addEnqueue']);
 
             add_action('admin_print_scripts-post-new.php', [$this,'cpt_admin_script']);
             add_action('admin_print_scripts-post.php', [$this,'cpt_admin_script']);
@@ -17,12 +18,29 @@ class StyleLoad {
         });
 
 	}
+   /*  public function addEnqueue()
+    {
+        
+        $current_screen = get_current_screen();
+
+        if(is_admin()){
+
+            if($current_screen->id == 'email' 
+                && $current_screen->base == 'post' 
+                && $current_screen->post_type == 'email')
+            {
+                wp_enqueue_style( 'emaiilkit-editor-style-cpt', EMAILKIT_URL . 'assets/dist/admin/styles/test.css', [], EMAILKIT_VERSION );
+                wp_enqueue_script( 'emailkit-metabox-scripts', EMAILKIT_URL . 'src/dest/merged.js', ['wp-plugins', 'wp-i18n', 'wp-element', 'wp-dom', 'wp-data'], EMAILKIT_VERSION, true );
+            }
+        }
+    } */
 
     function cpt_admin_script() {
        
         wp_enqueue_style( 'emaiilkit-editor-style-cpt', EMAILKIT_URL . 'assets/dist/admin/styles/test.css', [], EMAILKIT_VERSION );
-        wp_enqueue_script( 'emailkit-loadBtn-cpt', EMAILKIT_URL . 'src/dest/merged.js', ['wp-plugins','wp-edit-post', 'wp-i18n', 'wp-element', 'wp-dom', 'wp-data'], EMAILKIT_VERSION, true );
-}
+        wp_enqueue_script( 'emailkit-loadBtn-cpt', EMAILKIT_URL . 'src/dest/merged.js', ['wp-plugins','wp-edit-post', 'wp-i18n', 'wp-element', 'wp-dom', 'wp-data'], EMAILKIT_VERSION, true );  
+        
+    } 
     
 
 }
