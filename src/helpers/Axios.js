bb614c160eb3a6a4d389811abf98d6a99edf521c
xs-mostakim <mim@xpeedstudio.com>
2023-01-23 09:16:07 +0600

    src file deleted

--- src/helpers/Axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export const axiosInstance = axios.create({
-  baseURL: 'http://getgenie.com/xpeed/',
-  // baseURL: 'http://localhost/',
-});
--- src/next.config.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = {
-  assetPrefix: "./",
-  webpack5: true
-};
